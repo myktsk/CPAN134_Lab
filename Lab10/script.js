f5a13f067e4917c1966c2dc630abefe5e0566ecb
myktsk <22167483+myktsk@users.noreply.github.com>
2024-04-05 11:44:20 -0400

    make background color random

--- Lab10/script.js
+++ Lab10/script.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
     message.innerHTML = "Text changed successfully!";
   });
 
+  document.body.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+
   const newParagraph = document.createElement("p");
   newParagraph.innerHTML = "New paragraph added dynamically!";
   document.getElementById("container").appendChild(newParagraph);
